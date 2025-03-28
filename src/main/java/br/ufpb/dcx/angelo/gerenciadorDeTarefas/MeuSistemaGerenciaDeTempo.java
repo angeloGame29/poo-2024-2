49bf6f1c8766752548b31f334c5bd2c5ebc93696
João Ângelo <angelo.silva@dcx.ufpb.br>
2025-03-28 18:40:09 -0300

    add ITERAÇÃO INTERNA

--- src/main/java/br/ufpb/dcx/angelo/gerenciadorDeTarefas/MeuSistemaGerenciaDeTempo.java
+++ src/main/java/br/ufpb/dcx/angelo/gerenciadorDeTarefas/MeuSistemaGerenciaDeTempo.java
@@ -32,7 +32,9 @@ public class MeuSistemaGerenciaDeTempo implements SistemaGerenciaDeTempo {
 //        }
 //        return  tarefasPesquisadas;
         //=======================[ ITERAÇÃO INTERNA ]==============================
-        return this.tarefas.values().stream().filter(tarefa -> tarefa.getCategoria().equals(categoriaTarefa)).toList();
+        return this.tarefas.values().stream()
+                .filter(tarefa -> tarefa.getCategoria().equals(categoriaTarefa))
+                .toList();
         //========================[ EXPLICANDO PASSO A PASSO ]===============================================
         //this.tarefas.values() -> Obtém todas as tarefas do mapa.
         //stream() -> Converte tudo em um Stream, assim permitindo manipular tudo de forma declarativa.
@@ -49,29 +51,47 @@ public class MeuSistemaGerenciaDeTempo implements SistemaGerenciaDeTempo {
 //            }
 //        }
 //        return tarefasPesquisadas;
-        return this.tarefas.values().stream().filter(tarefa -> tarefa.getTempoTarefaEmMinutos() > tempoEmMinutos).toList();
+        return this.tarefas.values().stream()
+                .filter(tarefa -> tarefa.getTempoTarefaEmMinutos() > tempoEmMinutos)
+                .toList();
     }
 
     @Override
     public int calcularTempoTotalDeTarefasDaCategoria(CategoriaTarefa categoriaTarefa) {
-        int valorTotal = 0;
-        for (Tarefa t : this.tarefas.values()){
-            if (t.getCategoria().equals(categoriaTarefa)) {
-                valorTotal += t.getTempoTarefaEmMinutos();
-            }
-        }
-        return valorTotal;
+//        int valorTotal = 0;
+//        for (Tarefa t : this.tarefas.values()){
+//            if (t.getCategoria().equals(categoriaTarefa)) {
+//                valorTotal += t.getTempoTarefaEmMinutos();
+//            }
+//        }
+//        return valorTotal;
+        return this.tarefas.values().stream()
+                .filter(t -> t.getCategoria().equals(categoriaTarefa))
+                .mapToInt(Tarefa::getTempoTarefaEmMinutos)
+                .reduce(0, (a,b) -> a + b);
     }
 
     @Override
     public int calcularTempoTotalDeTarefasComDescricao(String descricaoTarefa) {
-        int valorTotal = 0;
-        for (Tarefa t : this.tarefas.values()){
-            if (t.getDescricao().equals(descricaoTarefa)) {
-                valorTotal += t.getTempoTarefaEmMinutos();
-            }
-        }
-        return valorTotal;
+//        int valorTotal = 0;
+//        for (Tarefa t : this.tarefas.values()){
+//            if (t.getDescricao().equals(descricaoTarefa)) {
+//                valorTotal += t.getTempoTarefaEmMinutos();
+//            }
+//        }
+//        return valorTotal;
+        //1) com o reduce:
+//      return this.tarefas.values().stream()
+//      .filter(t -> t.getDescricao().equals(descricaoTarefa))
+//      .mapToInt(Tarefa::getTempoTarefaEmMinutos)
+//      .reduce(0, (a,b) -> a + b);
+
+        //2) com o sum e sem usar o operador ::
+        return this.tarefas.values().stream()
+                .filter(t -> t.getDescricao().equals(descricaoTarefa))
+                .mapToInt(t -> t.getTempoTarefaEmMinutos())
+                .sum();
+//
     }
 
     @Override
@@ -83,15 +103,22 @@ public class MeuSistemaGerenciaDeTempo implements SistemaGerenciaDeTempo {
 //            }
 //        }
 //        return  tarefasPesquisadas;
-        return this.tarefas.values().stream().filter(tarefa -> tarefa.getDescricao().equals(descricaoTarefa)).toList();
+        return this.tarefas.values().stream()
+                .filter(tarefa -> tarefa.getDescricao().equals(descricaoTarefa))
+                .toList();
     }
 
     @Override
     public void alterarTempoDeTarefa(String codigoTarefa, int novoTempo) throws TarefaNaoExisteException {
-        if (!this.tarefas.containsKey(codigoTarefa)){
-            throw new TarefaNaoExisteException("Tarefa com o código: ["+codigoTarefa+"] não existe");
-        }else {
-            this.tarefas.get(codigoTarefa).setTempoTarefaEmMinutos(novoTempo);
-        }
+
+//        if (!this.tarefas.containsKey(codigoTarefa)){
+//            throw new TarefaNaoExisteException("Tarefa com o código: ["+codigoTarefa+"] não existe");
+//        }else {
+//            this.tarefas.get(codigoTarefa).setTempoTarefaEmMinutos(novoTempo);
+//        }
+        this.tarefas.values().stream()
+                .filter(t-> t.getCodigo().equals(codigoTarefa))
+                .findFirst().orElseThrow(()-> new TarefaNaoExisteException("Tarefa com o código: ["+codigoTarefa+"] não existe"))
+                .setTempoTarefaEmMinutos(novoTempo);
     }
 }
